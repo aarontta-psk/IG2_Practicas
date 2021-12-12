4423d701292fe5582cc98b58db27ce70f770a756
Psikotruko <62306290+SrVonPsikerfy@users.noreply.github.com>
2021-12-12 01:51:52 +0100

    Practica 4 - Apartado 13 Terminado

--- IG2_Practica4/media/IG2App/HolesAndLightingFS.glsl
+++ IG2_Practica4/media/IG2App/HolesAndLightingFS.glsl
@@ -4,28 +4,48 @@ uniform sampler2D textBumpyMetal; // ‐> unidades de textura (int)
 uniform sampler2D textBeachStones; // tipo sampler2D para texturas 2D
 uniform sampler2D textCorrosion; // ‐> unidades de textura (int)
 
+// uniform vec3 lightAmbient; // intensidades de la luz
+uniform vec3 lightDiffuse;
+uniform vec4 lightPosition;
+
+// uniform vec4 materialDiffuse;
+
+uniform vec3 ocreSiena;
+uniform vec3 azulCeruleo;
+
 // uniform float BF; // blending factor
 // uniform float intLuzAmb; // luz ambiente blanca
 
 in vec2 vUv0; // out del vertex shader
+in vec3 vCamNormal; // coordenadas de la normal en camera space
+// in vec4 vCamVertex; // coordenadas del vértice en camera space
 
 out vec4 fFragColor; // out del fragment shader
 
 void main() {
-    vec3 colorStones = vec3(texture(textBeachStones, vUv0)); // acceso a téxel
-    vec3 colorMetal = vec3(texture(textBumpyMetal, vUv0)); // configuración!
     vec3 colorCorrosion = vec3(texture(textCorrosion, vUv0)); // configuración!
 
-    vec3 ocresiena = vec3(0.72, 0.57, 0.35);
-    vec3 azulceruleo = vec3(0.0, 0.6, 0.83);
-
-    if (colorCorrosion.r <= 0.6)
-    {
-        if(gl_FrontFacing)
-            fFragColor = vec4(colorMetal * ocresiena, 1.0);
-        else
-            fFragColor = vec4(colorStones * azulceruleo, 1.0);
-    }
-    else                               
+    if(colorCorrosion.r > 0.6)
         discard;
+
+    if(gl_FrontFacing) {
+        vec3 colorMetal = vec3(texture(textBumpyMetal, vUv0)); // configuración!
+        colorMetal *= ocreSiena;
+
+        vec3 lightDir = normalize(vec3(lightPosition));
+        float diff = max(dot(vCamNormal, lightDir), 0.0); // positive normal
+        vec3 diffuse = vec3(lightDiffuse) * diff;
+
+        fFragColor = vec4(colorMetal * diffuse, 1.0);
+    } 
+    else {
+        vec3 colorStones = vec3(texture(textBeachStones, vUv0)); // acceso a téxel
+        colorStones *= azulCeruleo;
+        
+        vec3 lightDir = normalize(vec3(lightPosition));
+        float diff = max(dot(-vCamNormal, lightDir), 0.0); // negative normal
+        vec3 diffuse = vec3(lightDiffuse) * diff;
+
+        fFragColor = vec4(colorStones * diffuse, 1.0);
+    }
 }
--- IG2_Practica4/media/IG2App/HolesAndLightingVS.glsl
+++ IG2_Practica4/media/IG2App/HolesAndLightingVS.glsl
@@ -1,13 +1,21 @@
 #version 330 core
 
-in vec4 vertex; // atributos de los vértices a procesar
-in vec2 uv0; // coordenadas de textura 0
-uniform mat4 modelViewProjMat; // constante de programa
+in vec4 vertex;                     // atributos de los vertices a procesar
+in vec3 normal;
+in vec2 uv0;
 
-out vec2 vUv0; // out del vertex shader
+uniform mat4 modelViewProjMat;      // matriz de transformacion
+uniform mat4 normalMat;             // matriz inversa (normales)
+// uniform mat4 modelViewMat;      // matriz de transformacion
+
+out vec2 vUv0;                 // out del vertex shader
+out vec3 vCamNormal;
+// out vec4 vCamVertex; 
 
 void main() {
-    vUv0 = uv0; // se pasan las coordenadas de textura
-    gl_Position = modelViewProjMat * vertex; //obligatorio
-                                             // (Clipping coordinates)
+    vUv0 = uv0;
+    vCamNormal = normalize(mat3(normalMat) * normal);
+    // vCamVertex = vec3(modelViewMat * vertex);
+
+    gl_Position = modelViewProjMat * vertex;
 }
