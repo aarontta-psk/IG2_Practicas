b37256bcf6089e2ee9cd6a55960ded933b41ff5a
Nico <nrosa01@ucm.es>
2021-12-14 12:37:59 +0100

    Practica 4 -Arreglado shader holes and light

--- IG2_Practica4/media/IG2App/HolesAndLightingFS.glsl
+++ IG2_Practica4/media/IG2App/HolesAndLightingFS.glsl
@@ -4,23 +4,28 @@ uniform sampler2D textBumpyMetal; // ‐> unidades de textura (int)
 uniform sampler2D textBeachStones; // tipo sampler2D para texturas 2D
 uniform sampler2D textCorrosion; // ‐> unidades de textura (int)
 
-// uniform vec3 lightAmbient; // intensidades de la luz
+uniform vec3 lightAmbient; // intensidades de la luz
 uniform vec3 lightDiffuse;
 uniform vec4 lightPosition;
 
-// uniform vec4 materialDiffuse;
+uniform vec4 materialDiffuse;
 
 uniform vec3 ocreSiena;
 uniform vec3 azulCeruleo;
 
-// uniform float BF; // blending factor
-// uniform float intLuzAmb; // luz ambiente blanca
-
 in vec2 vUv0; // out del vertex shader
-in vec3 vCamNormal; // coordenadas de la normal en camera space
-// in vec4 vCamVertex; // coordenadas del vértice en camera space
+in vec3 vGlobalNormal; // coordenadas de la normal en camera space
+in vec4 vGlobalVertex; // coordenadas del vértice en camera space
 
 out vec4 fFragColor; // out del fragment shader
+
+float diff(vec3 vVertex, vec3 vNormal) {
+    vec3 lightDir = lightPosition.xyz; // directional light ?
+    if(lightPosition.w == 1) // positional light ?
+        lightDir = lightPosition.xyz - vVertex;
+        
+    return max(dot(vNormal, normalize(lightDir)), 0.0);
+}
 
 void main() {
     vec3 colorCorrosion = vec3(texture(textCorrosion, vUv0)); // configuración!
@@ -28,23 +33,22 @@ void main() {
     if(colorCorrosion.r > 0.6)
         discard;
 
+    vec3 ambient = lightAmbient * materialDiffuse.rgb;
+
     if(gl_FrontFacing) {
         vec3 colorMetal = vec3(texture(textBumpyMetal, vUv0)); // configuración!
-        colorMetal *= ocreSiena;
+        colorMetal *= (ocreSiena + ambient);
 
-        vec3 lightDir = normalize(vec3(lightPosition));
-        float diff = max(dot(vCamNormal, lightDir), 0.0); // positive normal
-        vec3 diffuse = vec3(lightDiffuse) * diff;
+        float diff = diff(vec3(vGlobalVertex), vGlobalNormal); // positive normal
+        vec3 diffuse = vec3(lightDiffuse) * diff * materialDiffuse.rgb;
 
         fFragColor = vec4(colorMetal * diffuse, 1.0);
-    } 
-    else {
+    } else {
         vec3 colorStones = vec3(texture(textBeachStones, vUv0)); // acceso a téxel
-        colorStones *= azulCeruleo;
-        
-        vec3 lightDir = normalize(vec3(lightPosition));
-        float diff = max(dot(-vCamNormal, lightDir), 0.0); // negative normal
-        vec3 diffuse = vec3(lightDiffuse) * diff;
+        colorStones *= (azulCeruleo  + ambient);
+
+        float diff = diff(vec3(vGlobalVertex), -vGlobalNormal); // negative normal
+        vec3 diffuse = vec3(lightDiffuse) * diff * materialDiffuse.rgb;
 
         fFragColor = vec4(colorStones * diffuse, 1.0);
     }
--- IG2_Practica4/media/IG2App/HolesAndLightingVS.glsl
+++ IG2_Practica4/media/IG2App/HolesAndLightingVS.glsl
@@ -6,16 +6,16 @@ in vec2 uv0;
 
 uniform mat4 modelViewProjMat;      // matriz de transformacion
 uniform mat4 normalMat;             // matriz inversa (normales)
-// uniform mat4 modelViewMat;      // matriz de transformacion
+uniform mat4 modelViewMat;      // matriz de transformacion
 
 out vec2 vUv0;                 // out del vertex shader
-out vec3 vCamNormal;
-// out vec4 vCamVertex; 
+out vec3 vGlobalNormal;
+out vec4 vGlobalVertex; 
 
 void main() {
     vUv0 = uv0;
-    vCamNormal = normalize(mat3(normalMat) * normal);
-    // vCamVertex = vec3(modelViewMat * vertex);
+    vGlobalNormal = normalize(mat3(normalMat) * normal);
+    vGlobalVertex = vec4(modelViewMat * vertex);
 
     gl_Position = modelViewProjMat * vertex;
 }
